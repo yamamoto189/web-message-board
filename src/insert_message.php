991b38ae7f6b2730d53a44312c0e4515dcc797a0
yamamoto189 <ming1107.000@gmail.com>
2024-06-14 14:04:22 +0900

    add6

--- src/insert_message.php
+++ src/insert_message.php
@@ -61,11 +61,17 @@ $stmt->bindValue(':message',$input_message,PDO::PARAM_STR);
 
  // クエリを実行する
  $stmt->execute();
+ $_SESSION['action_success_text']='投稿しました';
+ $_SESSION['action_error_text']='';
  $_SESSION['input_error_author_name']='';
  $_SESSION['input_error_message']='';
  $_SESSION['input_pre_author_name']='';
  $_SESSION['input_pre_message']='';
+}else{
+    $_SESSION['action_success_text']='';
+    $_SESSION['action_error_text']='入力内容を確認してください';
 }
 
 header('Location: /');
 exit();
+
